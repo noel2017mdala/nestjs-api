667697855cb48bd9ac519d29335a2d6e7339cc42
noel2017mdala <noelmdala2017@gmail.com>
2023-12-20 07:41:49 +0200

    addeed route guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guards';
 
 @Controller('users')
 export class UserController {
+  @UseGuards(JwtGuard)
   @Get('me')
-  getMe() {
-    return 'user information';
+  getMe(@Req() req: Request) {
+    // console.log({
+    //   user: req.user,
+    // });
+    return req.user;
   }
 }
